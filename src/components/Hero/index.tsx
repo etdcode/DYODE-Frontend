eccea6d88921c1e152cf6dc203bbf5be5141ab35
jamescoulterspace <jamescoulter.fulldev@gmail.com>
2022-06-24 15:32:48 -0400

    feat: hero carousel responsive

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -12,15 +12,45 @@ const Hero: React.FC = () => {
   return (
     <div className="hero">
       <Carousel
+        className='hero-lg'
         renderCenterLeftControls={({ previousSlide }) => (<></>)}
         renderCenterRightControls={({ nextSlide }) => (<></>)}
         autoplay={true}
         easing={"easeQuadInOut"}
         wrapAround={true}
         zoomScale={0.1}
+        dragging={false}
+        withoutControls={true}
+        defaultControlsConfig={{
+          pagingDotsStyle: {
+            fill: "white",
+            scale: 300,
+          }
+        }}
       >
         {images.map((step, idx) =>
           <div key={idx} className="hero-item" style={{ backgroundImage: `url(${step.image})` }}>
+            <div className="hero-content">
+              <p className={`hero-title ${step.color}`}>{step.title}</p>
+              <p className={`hero-subtitle ${step.color}`}>{step.subtitle}</p>
+              <div className='hero-button'>Shop Now</div>
+            </div>
+          </div>
+        )}
+      </Carousel>
+      <Carousel
+        className='hero-md'
+        renderCenterLeftControls={({ previousSlide }) => (<></>)}
+        renderCenterRightControls={({ nextSlide }) => (<></>)}
+        renderBottomCenterControls={() => (<></>)}
+        autoplay={true}
+        easing={"easeQuadInOut"}
+        wrapAround={true}
+        zoomScale={0.1}
+        dragging={false}
+      >
+        {images.map((step, idx) =>
+          <div key={idx} className="hero-item">
             <div className="hero-content">
               <p className={`hero-title ${step.color}`}>{step.title}</p>
               <p className={`hero-subtitle ${step.color}`}>{step.subtitle}</p>
